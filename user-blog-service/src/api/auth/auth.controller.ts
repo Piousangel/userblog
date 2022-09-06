7e926705e83dd7f5bacb64aabc20c68a8fd54610
Piousangel <yyh7654@gmail.com>
2022-09-07 08:51:37 +0900

    feat : controller, serivce 구현 #2

--- user-blog-service/src/api/auth/auth.controller.ts
+++ user-blog-service/src/api/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAu}
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private readonly authService: AuthService){}
+
+    /**
+     * 로그인 요청 -> JWT 토큰 발급
+     */
+    @UseGuards(LocalAuthGuard)
+    @Post('user/login')
+    async login(@Request() req){
+
+        return this.authService.login(req.user)
+    }
+
+    /**
+     *  JWT 검증 후에 사용자 정보 반환
+     */
+    @UseGuards(JwtA)
+    @Get('/profile')
+    getProfile(@Request() req) {
+        return req.user;
+    }
+}
--- user-blog-service/src/api/auth/auth.service.ts
+++ user-blog-service/src/api/auth/auth.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService,
+    ){}
+
+    /**
+     * DB에 email 존재 유무 검증
+     * email의 pw 정보와 입력정보 일치 여부 검증
+     * 
+     * @param email 로그인 id
+     * @param pass 로그인 password
+     */
+    async validateUser(email: string, password : string): Promise<any> {
+        const user = await this.userService.findOne(email);
+
+        if(user & user.password === password){
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    /**
+     * 로그인 요청 사용자에게 JWT access token 발급
+     * @param user 로그인을 요청하는 사용자
+     */
+    async login(user : any) {
+        const payload = { email: user.email, sub: user.id, grade : user.grade };
+
+        this.userService.update(user.id);
+
+        return {
+            access_token: this.jwtService.sign(payload);
+        }
+    }
+}
